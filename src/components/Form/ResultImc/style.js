768967c50b8e1cf78418cf58ccc92868c6607999
Larissa <annams123456@gmail.com>
2024-04-18 18:08:04 -0300

    first commit

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import ResultImc from ".";
+
+const styles = StyleSheet.create({
+    resultImc:{
+        flex: 1,
+        marginTop: 15,
+        marginTop: 60,  
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",
+    },
+    information:{
+        fontSize: 18,
+        color: "#010469",
+        fontWeight: "bold",
+    },
+    numberImc: {
+        fontSize: 48,
+        color: "#010469",
+        fontWeight: "bold",
+    }
+});
+
+export default styles 
